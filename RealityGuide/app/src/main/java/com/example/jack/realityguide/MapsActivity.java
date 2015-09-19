d3f5fed537abd01a7e41d02a9786a19597a71847
Jack Potato Le <nanotek2711@yahoo.com>
2015-09-19 03:17:56 -0400

    open gl test

---
+++ RealityGuide/app/src/main/java/com/example/jack/realityguide/MapsActivity.java
@@ -0,0 +1,97 @@
+package com.example.jack.realityguide;
+
+import android.content.Context;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.location.LocationManager;
+import android.location.Location;
+import android.location.LocationListener;
+
+public class MapsActivity extends FragmentActivity {
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    LocationManager theLocationManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        setUpMapIfNeeded();
+        theLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+    }
+
+    @Override
+    protected void onResume() throws SecurityException{
+        super.onResume();
+        setUpMapIfNeeded();
+        theLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+    }
+
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                    new LatLng(location.getLatitude(), location.getLongitude()), 17));
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+}
--- gyroscope/app/src/main/java/com/example/personal/gyroscope/Main.java
+++ gyroscope/app/src/main/java/com/example/personal/gyroscope/Main.java
@@ -7,23 +7,35 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
+import android.view.View;
 import android.widget.TextView;
+import android.graphics.Matrix;
+import android.widget.ImageView;
 
 public class Main extends Activity {
     TextView textX, textY, textZ;
     SensorManager sensorManager;
     Sensor sensor;
-
+    long previousTime = 0;
+    double oriZ = 0;
+    ImageView imageView;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageView = (ImageView) findViewById(R.id.imageView);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
 
         textX = (TextView) findViewById(R.id.textX);
         textY = (TextView) findViewById(R.id.textY);
         textZ = (TextView) findViewById(R.id.textZ);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                oriZ = 0;
+            }
+        });
     }
 
     public void onResume() {
@@ -45,9 +57,20 @@ public class Main extends Activity {
             float y = event.values[1];
             float z = event.values[2];
 
-            textX.setText("X : " + (int)x + " rad/s");
-            textY.setText("Y : " + (int)y + " rad/s");
-            textZ.setText("Z : " + (int)z + " rad/s");
+            textX.setText("X : " + (int)x);
+            textY.setText("Y : " + (int)y);
+            textZ.setText("Z : " + (int)z);
+            long currentTime = System.currentTimeMillis();
+            float deltaTime = currentTime - previousTime;
+            if (previousTime == 0) deltaTime = 0;
+            deltaTime /= 1000;
+            oriZ = Math.atan2(y, -x) * 180 / Math.PI - 90;
+//            oriZ = -x;
+            Matrix matrix = new Matrix();
+            imageView.setScaleType(ImageView.ScaleType.MATRIX);   //required
+            matrix.setRotate((float) oriZ, imageView.getWidth()/2, imageView.getHeight()/2);
+            imageView.setImageMatrix(matrix);
+//            previousTime = currentTime;
         }
     };
 }
